e7e9179a06e1cceafcd338641c477d27a3ae34a4
NickAnaya618 <anaya.nicholas@gmail.com>
2017-03-21 21:54:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = 'my_access_token_that_im_keeping_private_whatsup'
+
+fetch('https://api.github.com/repos/jquery/jquery/commits',
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
